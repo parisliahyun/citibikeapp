5200641f786c725eb42ca479d79e440ce1a17306
My Name <myemail@example.com>
2013-10-22 12:36:06 -0400

    working code on algorithm refactored

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,49 +1,31 @@
 class SearchesController < ApplicationController
 
-
   def index
     @coordinates = Geocoder.coordinates(params[:address])
-      # need if statement to avoid error
-        @x = @coordinates[0]
-        @y = @coordinates[1]
+
+    unless @coordinates.nil?
+      
+      @coordinates = { "latitude" => @coordinates[0], "longitude" => @coordinates[1] }
+      @citibike_docks = Citibikenyc.branches.values[2]
+
+      @distances = @citibike_docks.map do |dock|
+      @distance_x = @coordinates["longitude"] - dock["longitude"]
+      @distance_y = @coordinates["latitude"] - dock["latitude"]
+
+      @distance_to_one_dock = Math.hypot( @distance_x, @distance_y )
+      [dock["label"], @distance_to_one_dock]
+      end
+
+      binding.pry
+
+      #@distances.min
+      #@final_glorious_answer = Geocoder.address(@closest_dock)
  
-        @x2 = []
-        Citibikenyc.branches.values[2].each do |station_location| 
-        @x2 <<  station_location.values[1]
-        end
-
-        @y2 = []
-        Citibikenyc.branches.values[2].each do |station_location| 
-          @y2 <<  station_location.values[2]
-        end
-
-        @docks = []
-        Citibikenyc.branches.values[2].each do |dock| 
-          @docks <<  dock.values[3]
-        end
-
-        @arrays_of_dock_coordinates = @x2.zip(@y2)
-
-  
-        @distances = []
-        @arrays_of_dock_coordinates.each do |lat, long|
-        @distances << Math.hypot(@x - lat,@y - long)
-
-        @result = @distances.min
-        i = @distances.index(@result)
-       @closest_dock = @arrays_of_dock_coordinates[i] 
-        end
-
-      @final_glorious_answer = Geocoder.address(@closest_dock)
-
-        
-        # @available_dock = the lat and long of @result turned into the address. 
-
       render :index
+    else
+      render erb: "<%= debug Geocoder.coordinates(params[:address]) %>"
     end
 
-  # def results
-  #   @coordinates
-  #   render :results
-  # end  
+  end
+
 end
