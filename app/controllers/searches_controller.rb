4510313def86f8798a53cf8752d6fbe7b7372f9d
My Name <myemail@example.com>
2013-10-24 14:40:11 -0400

    bikes and docks if else search working code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,40 +1,41 @@
 class SearchesController < ApplicationController
 
-# TODO if winning_dock's available_docks == 0 within .1 miles of user's position, send notification to reroute with address to available dock.  
-# twitter
-# newsie 
-# suggestion box
-# about page
-# find a bike. DONE
-# review account route and about route make sure it's in the right view. DONE   
-
-
   def new
     render :new
   end
 
   def create
     @coordinates = Geocoder.coordinates(params[:address])
-    @dock = params[:dock] == "Find me a dock"
     
     unless @coordinates.nil?
-      @coordinates = { latitude: @coordinates[0], longitude: @coordinates[1] }
 
+      @coordinates = { latitude: @coordinates[0], longitude: @coordinates[1] }
       all_citibike_stations = Citibikenyc.stations.values[2]
-      if @dock  
-        available_stations = all_citibike_stations.reject { |d| d["availableDocks"] == 0 }
-      else
-        available_stations = all_citibike_stations.reject { |d| d["availableBikes"] == 0 }
-      end  
-      @winning_station = available_stations.min_by do |station|
+
+      available_stations_for_docks = all_citibike_stations.reject { |d| d["availableDocks"] == 0 }
+
+      available_stations_for_bikes = all_citibike_stations.reject { |d| d["availableBikes"] == 0 } 
+
+      @winning_dock_station = available_stations_for_docks.min_by do |station|
         distance_x = @coordinates[:longitude] - station["longitude"]
         distance_y = @coordinates[:latitude] - station["latitude"]
         Math.hypot( distance_x, distance_y )
       end 
-      render :results
-    else
-      render erb: "<%= debug Geocoder.coordinates(params[:address]) %>"
-    end
-  end
 
-end
+      @winning_bike_station = available_stations_for_bikes.min_by do |station|
+        distance_x = @coordinates[:longitude] - station["longitude"]
+        distance_y = @coordinates[:latitude] - station["latitude"]
+        Math.hypot( distance_x, distance_y )
+      end 
+
+     render :results
+    else
+     render erb: "<%= debug Geocoder.coordinates(params[:address]) %>"  
+    end
+ end   
+end 
+
+
+
+
+
